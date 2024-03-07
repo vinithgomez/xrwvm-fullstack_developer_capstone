fc234f65681f5710b434e097c33c4d5835119393
digital-installer <digitalupgradeintelligence@gmail.com>
2024-03-07 09:51:17 +0000

    Resolved issues on the push and ways

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,10 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarModel, CarMake
 
 
 # Register your models here.
+admin.site.register(CarModel)
+admin.site.register(CarMake)
 
 # CarModelInline class
 
