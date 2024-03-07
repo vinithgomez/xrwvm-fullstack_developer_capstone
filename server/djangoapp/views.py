a0324165c45ba7c43de16c64b72b973dee2ddc88
digital-installer <digitalupgradeintelligence@gmail.com>
2024-03-07 16:30:19 +0000

    Resolved issues on the push and ways

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -112,7 +112,6 @@ def get_dealer_reviews(request, dealer_id):
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
-            print(response)
             review_detail['sentiment'] = response['sentiment']
         return JsonResponse({"status":200,"reviews":reviews})
     else:
