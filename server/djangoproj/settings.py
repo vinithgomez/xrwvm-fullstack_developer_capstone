d130e98b5e1c3ffc3fb14c4f8ddec61f493d8054
Vinith Gomez <147228500+vinithgomez@users.noreply.github.com>
2025-10-05 12:02:25 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,11 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', "https://vinithgomez3-8000."
-               "theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy"
-               ".cognitiveclass.ai"]
+                 "theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy"
+                 ".cognitiveclass.ai"]
 CSRF_TRUSTED_ORIGINS = ['https://vinithgomez3-8000.'
-                      'theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy'
-                      '.cognitiveclass.ai']
+                        'theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy'
+                        '.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
